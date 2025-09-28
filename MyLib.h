e4ee0003847bee68011f18f5e2022071b437353f
ErNeStAs-VVK <ernestas.padvorskis@vvk.lt>
2025-09-28 18:25:49 +0300

    Create MyLib.h

---
+++ MyLib.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+using std::cout;
+using std::string;
+using std::vector;
+using std::accumulate;
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Studentas {
+    string vardas;
+    string pavarde;
+    vector<int>paz;
+    int egzaminas;
+    double rezultatas;
+public:
+    Studentas();
+    Studentas(string A, string B, vector<int> C, int D);
+    void rez();
+    ~Studentas();
+    void output();
+    friend std::ostream& operator<<(std::ostream& os, const Studentas& obj);
+    friend std::istream& operator>>(std::istream& is, Studentas& obj);
+};
