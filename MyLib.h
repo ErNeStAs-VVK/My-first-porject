7f9890f0cec0194c6c96431e420b6054edfcccb2
ErNeStAs-VVK <ernestas.padvorskis@vvk.lt>
2025-10-02 15:01:14 +0300

    Update MyLib.h

--- MyLib.h
+++ MyLib.h
@@ -1,30 +1,47 @@
 #pragma once
-
 #include <iostream>
 #include <string>
 #include <vector>
-#include <numeric>
-using std::cout;
-using std::string;
-using std::vector;
-using std::accumulate;
-using std::cout;
-using std::cin;
-using std::endl;
+
 
 class Studentas {
-    int a
-    string vardas;
-    string pavarde;
-    vector<int>paz;
-    int egzaminas;
-    double rezultatas;
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> paz;   
+    int egzaminas = 0;      
+    double rezultatas = 0.0;
+
 public:
-    Studentas();
-    Studentas(string A, string B, vector<int> C, int D);
-    void rez();
-    ~Studentas();
-    void output();
+
+    Studentas() = default;
+    Studentas(const Studentas&) = default;
+    Studentas& operator=(const Studentas&) = default;
+    ~Studentas() = default;
+
+    Studentas(std::string A, std::string B, std::vector<int> C, int D);
+
+
+    void skaiciuoti(bool pagalMediana); 
+    void rez(); 
+
+
     friend std::ostream& operator<<(std::ostream& os, const Studentas& obj);
     friend std::istream& operator>>(std::istream& is, Studentas& obj);
+
+    // Getteriai
+    const std::string& Vardas()   const { return vardas; }
+    const std::string& Pavarde()  const { return pavarde; }
+    const std::vector<int>& Paz() const { return paz; }
+    int Egz() const { return egzaminas; }
+    double Galutinis() const { return rezultatas; }
 };
+
+
+double vidurkis(const std::vector<int>& v);
+double mediana(std::vector<int> v);
+
+
+std::vector<Studentas> SkaitytiIsFailo(const std::string& path);
+
+
+std::vector<Studentas> SugeneruotiStudentus(int N, int nd_kiek);
